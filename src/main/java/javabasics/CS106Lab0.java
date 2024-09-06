dc6931676b73621bbf7ef44f95674248882dd83c
Lauren Martin <lnmartin@haverford.edu>
2024-09-06 12:53:45 -0400

    first commit

---
+++ src/main/java/javabasics/CS106Lab0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package javabasics;
+
+/**
+ *
+ * @author laurenmartin
+ */
+public class CS106Lab0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
